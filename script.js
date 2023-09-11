71b4cf7e6dabc036ec173a25a8a93869c966375d
--replace-all <amajid0981@gmail.com>
2023-09-11 08:16:45 +0200

    worked around the changing of colours

--- script.js
+++ script.js
@@ -32,59 +32,84 @@ function removeAllChildNodes() {
 
 function toggleSize() {
     const form = document.querySelector('form')
+    let colour;
     form.addEventListener('submit', function (e) {
         removeAllChildNodes()
         e.preventDefault()
         size = form.elements['t'].value
+        colour = getSketchColour()
+        console.log("Outside: " +colour)
         creatGrid(size)
         useEraser(size)
         sketch(size)
+        pickRandomColour(size)
         hover(size)
     })
 }
 function hover(dimension) {
     const squares = document.querySelectorAll('.square')
     console.log(squares)
+    let color = getSketchColour()
     let calcHeight = sketch_height/dimension
     let calcWidth = sketch_width/ dimension
     squares.forEach(square=> {
         square.addEventListener('mouseover', function(e) {
 
             e.target.style.cssText = `outline: 1px solid black;height:${calcHeight}px;
-            width: ${calcWidth}px;background-color:orange;`
+            width: ${calcWidth}px;background-color:${color};`
 
 
         })
     })
 }
 
-function eraser(dimension) {
-    const squares = document.querySelectorAll('.square')
-    let calcHeight = sketch_height/dimension
-    let calcWidth = sketch_width/ dimension
-    squares.forEach(square => {
-        square.addEventListener('mouseover', function(e) {
-            e.target.style.cssText = `outline: 1px solid black;height:${calcHeight}px;
-            width: ${calcWidth}px;background-color:white;`
-        })
-    })
-    console.log('eraser')
-}
+
 
 function useEraser(dimension) {
     let button = document.querySelector('.erase')
     button.addEventListener('click', function(e) {
-        eraser(dimension)
+        let white = 'beige'
+        hover(dimension, white)
     })
 }
+
+
+
+function getSketchColour() {
+    let button = document.querySelector('#pick-color')
+    
+    let color = button.value
+
+    button.addEventListener('mouseover', function(e) {
+        console.log(e)
+        color = button.value
+        console.log(color)
+        return color
+    })
+    return color
+}
+
 
 function sketch(dimension) {
     let button = document.querySelector('.draw')
-    
-    button.addEventListener('click', function(e) {
+    button.addEventListener('click', function() {
         hover(dimension)
     })
 }
+
+
+
+function pickRandomColour(dimension) {
+    let button = document.querySelector('.random')
+    let r, g, b;
+    let colour
+    button.addEventListener('click', function(e) {
+        r = Math.random()* 255; g = Math.random()* 255; b = Math.random() * 255
+
+        colour = `rgb(${r}, ${g}, ${b})`
+        hover(dimension, colour)
+    })
+    
+}
+
 toggleSize()
-
-
